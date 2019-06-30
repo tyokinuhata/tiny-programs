07926e7ca3f6d7a8c1630f5b4f77692b51e810b4
Kazuki Tobita <kazukiti201@gmail.com>
2019-07-01 03:02:16 +0900

    Add strchr

---
+++ c/strchr/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    char s1[] = "hello, world!";
+
+    // strchr
+    // 文字列の検索
+    // strchr(検索対象文字列, 検索文字列)
+    char *s2 = strchr(s1, 'w');
+
+    printf("%s\n", s2);
+
+    return 0;
+}
