a946c47364fe6c3f2a4401f76e6fd7d1200c99c8
Kazuki Tobita <kazukiti201@gmail.com>
2019-07-02 16:11:06 +0900

    Modify void_pointer

--- c/void_pointer/main.c
+++ c/void_pointer/main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-void outputString(void *);
+void output_string(void *);
 
 int main(int argc, char *argv[]) {
     int n = 65;
     double f = 3.14;
 
-    outputString(&n);
-    outputString(&f);
+    output_string(&n);
+    output_string(&f);
 
     return 0;
 }
 
 // voidポインタ
 // どんなデータ型のポインタでも格納できる
-void outputString(void *v) {
+void output_string(void *v) {
     // もちろんサイズは違うので結果は保証されない
     // ただエラーは出ない
     char *s = (char *)v;
