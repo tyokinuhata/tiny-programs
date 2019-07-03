86a95678c8071debf6f913c528b49d2cd0deb11c
Kazuki Tobita <kazukiti201@gmail.com>
2019-07-03 14:35:13 +0900

    Modify list

--- c/list/main.c
+++ c/list/main.c
@@ -4,24 +4,25 @@
 typedef struct tag_list {
     int key;
     char *value;
-    struct tag_list *next; // 次のリストを示すポインタ
+    struct tag_list *next; // 次の要素を示すポインタ
 } t_list;
 
-t_list *head = NULL;
+t_list *current = NULL;
 void *start = NULL;
 
-// データ追加
+// 要素追加
 void add(int, char *);
-// データの検索
+// 要素の検索
 struct tag_list *find();
-// データ削除
+// 要素削除
 int delete(int);
-// 全データ表示
+// 全要素表示
 void show(void);
-// 全データ削除
+// 全要素削除
 int truncate(void);
 
 int main(int argc, char *argv[]) {
+    // 要素の追加
     add(1, "foo");
     add(2, "bar");
     add(3, "baz");
@@ -33,35 +34,35 @@ int main(int argc, char *argv[]) {
         list = list->next;
     }
 
-    // printf("%s\n", head->value);
-
     return 0;
 }
 
 void add(int key, char *value) {
     // 先頭要素がNULLの場合
-    if (head == NULL) {
-        // ヘッドの作成
-        head = malloc(sizeof(t_list));
+    if (current == NULL) {
+        // メモリ確保
+        current = malloc(sizeof(t_list));
 
         // データの挿入
-        head->key = key;
-        head->value = value;
-        head->next = NULL;
+        current->key = key;
+        current->value = value;
+        current->next = NULL;
 
-        start = head;
+        // 先頭要素のアドレスを保存
+        start = current;
     }
     else {
-        // 古いヘッドを退避
-        t_list *old_head = head;
+        // 現在の位置を移動
+        t_list *old = current;
 
-        // 新しいヘッドの作成
-        head = malloc(sizeof(t_list));
-        head->key = key;
-        head->value = value;
-        head->next = NULL;
+        // 現在位置にデータを挿入
+        current = malloc(sizeof(t_list));
+        current->key = key;
+        current->value = value;
+        current->next = NULL;
 
-        old_head->next = head;
+        // 一つ前の要素のnextに現在の要素のアドレスを挿入
+        old->next = current;
     }
 }
 
