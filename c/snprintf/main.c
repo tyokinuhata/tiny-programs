d8eb81b5d1973daba013a5bc1f83a0a73d151dec
Kazuki Tobita <kazukiti201@gmail.com>
2019-07-17 00:47:56 +0900

    Modify snprintf

--- c/snprintf/main.c
+++ c/snprintf/main.c
@@ -6,11 +6,15 @@ int main(int argc, char *argv[]) {
     char s3[] = "bar";
 
     // printf関数は標準出力されてしまうが, snprintfを使えば変数に入れておくことができる
+    // C99で追加
     // snprintf(出力先, 出力先のサイズ, 書式, 変数１, 変数２, ...);
     snprintf(s1, sizeof(s1), "%s%s", s2, s3);
 
     // 簡略化されたバージョンにsprintf関数もある
+    // こちらはバッファオーバーフローの脆弱性がある
     // sprintf(出力先, 書式, 変数１, 変数２, ...)
+
+    // C11からはsprintf_s関数も追加された
 
     printf("%s\n", s1);
 
