cd9852b8da21fa951203e803bade067bb02dbfbd
Kazuki Tobita <kazukiti201@gmail.com>
2019-07-18 09:56:04 +0900

    Modify struct

--- c/struct/main.c
+++ c/struct/main.c
@@ -39,5 +39,59 @@ int main(int argc, char *argv[]) {
     printf("%f\n", (*pcoord).x);
     printf("%f\n", (*pcoord).y);
 
+    // 自己参照構造体
+    typedef struct tag_foo {
+        // something ...
+
+        // typedefが完了していないのでt_foo型は使えない
+        // また, タグのみを宣言した場合, その値は不完全型となりポインタをとることしかできない
+        struct tag_foo *foo;
+    } t_foo;
+
+    // or
+
+    typedef struct tag_bar t_bar;
+
+    struct tag_bar {
+        // something ...
+        struct t_bar *bar;
+    };
+
+    // 構造体の相互参照
+    typedef struct tag_man t_man;
+
+    typedef struct {
+        t_man *man;
+    } t_woman;
+
+    struct tag_man {
+        t_woman *woman;
+        // t_woman woman; // t_womanは構造体の中身も定義しているので実体をとることもできる
+    };
+
+    // 構造体のネスティング
+    typedef struct {
+        // something ...
+    } t_baz;
+
+    typedef struct {
+        // something ...
+        t_baz baz;
+    } t_qux;
+
+    typedef struct {
+        // something ...
+        struct tag_corge {
+            // something ...
+        } t_corge;
+    } t_quux;
+
+    typedef struct {
+        // something ...
+        struct {
+            // something ...
+        } t_garply;
+    } t_grault;
+
     return 0;
 }
