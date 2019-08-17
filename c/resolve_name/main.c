d7f9c6d59d9572ae5a1da36154245dde8bd43795
Kazuki Tobita <kazukiti201@gmail.com>
2019-08-18 05:58:15 +0900

    Modify resolve_name

--- c/resolve_name/main.c
+++ c/resolve_name/main.c
@@ -3,7 +3,7 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-int resolve_name(char *, struct in_addr *);
+struct in_addr *resolve_name(char *);
 
 // ./a.out google.com
 int main (int argc, char **argv)
@@ -13,23 +13,24 @@ int main (int argc, char **argv)
     // ホスト名
     char *host_name = argv[1];
 
-    struct in_addr addr;
-    if (resolve_name(host_name, &addr) < 0) exit(EXIT_FAILURE);
+    struct in_addr *addr;
+    if ((addr = resolve_name(host_name)) == NULL) exit(EXIT_FAILURE);
 
-    printf("%s\n", inet_ntoa(addr));
+    printf("IP Address: %s\n", inet_ntoa(*addr));
 }
 
 // 名前解決
-int resolve_name (char *host_name, struct in_addr *addr)
+struct in_addr *resolve_name (char *host_name)
 {
     struct hostent *host;
     // gethostbyname関数
     // ホスト名を引数として取り, IPアドレス等のホスト情報を返す関数
     // struct hostent *gethostbyname(const char *hostName)
     // 戻り値はhostent構造体
-    if ((host = gethostbyname(host_name)) == NULL) return -1;
+    if ((host = gethostbyname(host_name)) == NULL) return NULL;
 
-    addr->s_addr = *((unsigned long *)host->h_addr_list[0]);
+    static struct in_addr addr;
+    addr.s_addr = *((unsigned long *)host->h_addr_list[0]);
 
-    return 0;
+    return &addr;
 }
