de401d891ddab6cfe405eb627219b43042d35a54
Kazuki Tobita <kazukiti201@gmail.com>
2019-08-28 13:35:08 +0900

    Modify bpf

--- c/bpf/main.c
+++ c/bpf/main.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <err.h>
 #include <errno.h>
@@ -35,6 +36,7 @@ int main (int argc, char **argv)
         // int snprintf(char *restrict s, size_t n, const char *restrict format, ...)
         // formatが示す書式文字列に従ってsが指す文字配列へ書き込みを行う
         snprintf(path, sizeof(path), "/dev/bpf%d", i);
+        // open関数
         // int open(const char *pathname, int flags, mode_t mode)
         // pathnameに対応するファイルディスクリプタが作成され, そのファイルディスクリプタが返される
         // O_RDWR ... 読み書き両用でオープンする
@@ -42,7 +44,7 @@ int main (int argc, char **argv)
         fd = open(path, O_RDWR, 0);
         if (fd != -1) break;
     }
-    if (fd < 0) err(EXIT_FAILURE, "open");
+    if (fd == -1) err(EXIT_FAILURE, "open");
 
     char *if_name = argv[1];
     struct ifreq if_req;
@@ -50,12 +52,14 @@ int main (int argc, char **argv)
     //              どちらも16をdefineしたもの
     //              前者はif_var.h, 後者はif.hで定義されていた
     strncpy(if_req.ifr_name, if_name, IFNAMSIZ - 1);
+    // BIOCSETIF ... インタフェースの設定
     if (ioctl(fd, BIOCSETIF, &if_req) < 0) {
         perror("ioctl [BIOCSETIF]");
         close(fd);
         exit(EXIT_FAILURE);
     }
     unsigned int size;
+    // BIOCGBLEN ... bpfファイル上で読み込むために必要なバッファ長を取得する
     if (ioctl(fd, BIOCGBLEN, &size) < 0) {
         perror("ioctl [BIOCGBLEN]");
         close(fd);
@@ -67,6 +71,7 @@ int main (int argc, char **argv)
         close(fd);
         exit(EXIT_FAILURE);
     }
+    // BIOCPROMISC ... 強制的にプロミスキャスモードに変更する(全てのインタフェースのパケットを取得できるようにする)
     if (ioctl(fd, BIOCPROMISC, NULL) , 0) {
         perror("ioctl [BIOCPROMISC]");
         free(buf);
@@ -74,12 +79,16 @@ int main (int argc, char **argv)
         exit(EXIT_FAILURE);
     }
     int enable = 1;
+    // BIOCIMMEDIATE ... 直接モードを有効 or 無効にする
+    //                   有効: read関数はパケットの受信でただちに返る
+    //                   無効: read関数はバッファがいっぱいになるか, タイムアウトが起きるまでブロックする
     if (ioctl(fd, BIOCIMMEDIATE, &enable) < 0) {
         perror("ioctl [BIOCIMMEDIATE]");
         free(buf);
         close(fd);
         exit(EXIT_FAILURE);
     }
+    // BIOCSHDRCMPLT ... ヘッダ完全フラグを有効 or 無効にする
     if (ioctl(fd, BIOCSHDRCMPLT, &enable) < 0) {
         perror("ioctl [BIOCSHDRCMPLT]");
         free(buf);
@@ -88,6 +97,9 @@ int main (int argc, char **argv)
     }
     ssize_t recv_size;
     for (;;) {
+        // read関数
+        // ssize_t read(int fd, void *buf, size_t count)
+        // 指定したファイルディスクリプタから内容を最大countバイトをbufへ読み込む
         recv_size = read(fd, buf, size);
         if (recv < 0) {
             if (errno == EINTR) continue;
@@ -99,7 +111,9 @@ int main (int argc, char **argv)
         unsigned int offset = 0;
         struct bpf_hdr *handler = (struct bpf_hdr *)buf;
         while ((uintptr_t)handler < (uintptr_t)buf + recv_size) {
-            printf("read [%du]: %du bytes via %s\n", offset++, handler->bh_caplen, if_name);
+            printf("read [%d]: %d bytes via %s\n", offset++, handler->bh_caplen, if_name);
+            // BPF_WORDALIGNマクロ関数
+            // ワード境界を考慮したパディング分のサイズを付加してくれる
             handler = (struct bpf_hdr *)((uintptr_t)handler + BPF_WORDALIGN(handler->bh_hdrlen + handler->bh_caplen));
         }
     }
