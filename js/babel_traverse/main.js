875a861bc8e753a2f1775e7b2c8989b1a95397d4
Kazuki Tobita <kazukiti201@gmail.com>
2019-10-28 11:07:14 +0900

    Modify babel_traverse

--- js/babel_traverse/main.js
+++ js/babel_traverse/main.js
@@ -1,9 +1,25 @@
 const babylon = require('babylon');
 const traverse = require('@babel/traverse').default;
 
-const ast = babylon.parse('1 + 2');
+const src = '1 + 2';
+
+const ast = babylon.parse(src);
 
 const visitor = {
+    enter(nodePath) {
+        console.log(`enter: ${nodePath.type}`);
+    },
+    exit(nodePath) {
+        console.log(`exit: ${nodePath.type}`);
+    },
+    NumericLiteral: {
+        enter(nodePath) {
+            console.log(`enter: ${nodePath.type}`);
+        },
+        exit(nodePath) {
+            console.log(`exit: ${nodePath.type}`);
+        }
+    },
     BinaryExpression: (nodePath) => {
         console.log(nodePath.node);
     }
