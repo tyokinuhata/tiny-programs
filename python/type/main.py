1a1ff3682193e297b164d2bbfffa00a7c425a7fa
Kazuki Tobita <kazukiti201@gmail.com>
2019-11-10 18:56:15 +0900

    Add type

---
+++ python/type/main.py
@@ -0,0 +1,24 @@
+# int型
+i = 256
+print(i)
+print(type(i))
+
+# str型
+s = 'string'
+print(s)
+print(type(s))
+
+# float型
+f = 3.14
+print(f)
+print(type(f))
+
+# tuple型
+t = (1, 2, 3, 4, 5)
+print(t)
+print(type(t))
+
+# list型
+l = [1, 2, 3, 4, 5]
+print(l)
+print(type(l))
