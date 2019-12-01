1154b64f44f66b2d1b046359a05c92e76d5da7ec
Kazuki Tobita <kazukiti201@gmail.com>
2019-12-02 01:38:34 +0900

    Add matplotlib_line_graph

---
+++ python/matplotlib_line_graph/main.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.plot([1, 2, 3, 4])
+plt.show()
+
+plt.plot([4, 3, 2, 1])
+plt.show()
+
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([100, 200, 300, 400, 500])
+plt.plot(x, y)
+plt.show()
+
