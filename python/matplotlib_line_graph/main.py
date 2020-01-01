90f20cfd8268dda15af463254ff49a33547a1de5
Kazuki Tobita <kazukiti201@gmail.com>
2020-01-01 19:54:40 +0900

    Add matplotlib_line_graph

--- python/matplotlib_line_graph/main.py
+++ python/matplotlib_line_graph/main.py
@@ -12,3 +12,14 @@ y = np.array([100, 200, 300, 400, 500])
 plt.plot(x, y)
 plt.show()
 
+x = np.arange(5)
+y = np.arange(start = 100, stop = 600, step = 100)
+plt.plot(x, y)
+plt.show()
+
+vol = 30
+x = np.arange(vol)
+y = [x_i + np.random.randn(1) for x_i in x]
+a, b = np.polyfit(x, y, 1)
+plt.plot(x, y, 'o', np.arange(vol), a * np.arange(vol) + b, '-')
+plt.show()
