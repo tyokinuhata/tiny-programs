61811355bbafc28fe43ce5b0ecb7c0a328a15f15
Kazuki Tobita <kazukiti201@gmail.com>
2020-01-03 02:42:42 +0900

    Modify sklearn_iris

--- python/sklearn_iris/main.py
+++ python/sklearn_iris/main.py
@@ -5,5 +5,11 @@ from sklearn import datasets
 # データセットの読み込み
 iris = datasets.load_iris()
 
-# 特徴量の確認
+# 特徴量の種類
 # print(iris.feature_names)
+
+# 学習データ
+# print(iris.data)
+
+# ラベルデータ
+# print(iris.target)
