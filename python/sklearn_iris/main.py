900e6626a83ee94d2850185442b75dc665acc46e
Kazuki Tobita <kazukiti201@gmail.com>
2020-01-03 19:10:50 +0900

    Modify sklearn_iris

--- python/sklearn_iris/main.py
+++ python/sklearn_iris/main.py
@@ -17,6 +17,22 @@ model = SVC(C = 1.0, kernel = 'linear')
 model.fit(data, labels)
 
 # 回帰係数
-print(model.coef_)
+# print(model.coef_)
 # 切片(誤差)
-print(model.intercept_)
+# print(model.intercept_)
+
+fig, ax = plt.subplots(figsize = (12, 9))
+
+setoa = data[labels == 0]
+versicolor = data[labels == 1]
+
+plt.scatter(setoa[:, 0], setoa[:, 1], s = 300, c = 'white', linewidths = 2, edgecolor = 'lightgray')
+plt.scatter(versicolor[:, 0], versicolor[:, 1], s = 300, c = 'firebrick', linewidths = 2, edgecolor = 'lightgray')
+
+Xi = np.linspace(4, 7.25)
+Y = -model.coef_[0][0] / model.coef_[0][1] * Xi - model.intercept_ / model.coef_[0][1]
+
+ax.plot(Xi, Y, linestyle = 'dashed', linewidth = 3)
+
+plt.grid(True)
+plt.show()
