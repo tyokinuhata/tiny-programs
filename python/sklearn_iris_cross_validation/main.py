709909b81d01f289e8cee2bde1ed192010a6f52c
Kazuki Tobita <kazukiti201@gmail.com>
2020-01-04 08:49:18 +0900

    Modify sklearn_iris_cross_validation

--- python/sklearn_iris_cross_validation/main.py
+++ python/sklearn_iris_cross_validation/main.py
@@ -3,6 +3,7 @@ import sklearn.datasets as datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 iris = datasets.load_iris()
 
@@ -21,4 +22,15 @@ test_data_std = sc.transform(test_data)
 
 # 学習
 model = SVC(kernel = 'linear', random_state = None)
-model.fit(train_data, train_labels)
+model.fit(train_data_std, train_labels)
+
+# 学習済モデルに学習データを分類させる際の精度
+predict_train = model.predict(train_data_std)
+# 分類精度(正解率)の算出
+accuracy_train = accuracy_score(train_labels, predict_train)
+print('%.2f' % accuracy_train)
+
+# 学習済みモデルに学習データを分類させる際の精度
+predict_test = model.predict(test_data_std)
+accuracy_test = accuracy_score(test_labels, predict_test)
+print(accuracy_test)
