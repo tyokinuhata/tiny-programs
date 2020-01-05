1b03761b73c2ef412f60a6cfd6220a7a802aa8ef
Kazuki Tobita <kazukiti201@gmail.com>
2020-01-06 02:23:56 +0900

    Modify chainer_mnist

--- python/chainer_mnist/main.py
+++ python/chainer_mnist/main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import chainer
 
+# MNIST(Mixed National Institute of Standards and Technology database)
+# 手書き数字のデータセットが70,000個ある
+# 28 x 28 = 784次元のデータ
+# scikit-learnの手書き数字は8 x 8のため, こちらの方がより特徴量が多くなっている
 train_data, test_data = chainer.datasets.get_mnist(withlabel = True, ndim = 1)
 print(train_data)
 print(test_data)
