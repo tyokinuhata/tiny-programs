b1fa40d68591b9e71544e624dd9e07fe781b1d53
Kazuki Tobita <kazukiti201@gmail.com>
2020-01-07 01:44:30 +0900

    Modify chainer_mnist

--- python/chainer_mnist/main.py
+++ python/chainer_mnist/main.py
@@ -3,15 +3,7 @@ import chainer
 import chainer.links as L
 import chainer.functions as F
 
-# MNIST(Mixed National Institute of Standards and Technology database)
-# 手書き数字のデータセットが70,000個ある
-# 28 x 28 = 784次元のデータ
-# scikit-learnの手書き数字は8 x 8のため, こちらの方がより特徴量が多くなっている
-# train_data, test_data = chainer.datasets.get_mnist(withlabel = True, ndim = 1)
-# print(train_data)
-# print(test_data)
-# print(len(train_data))
-# print(len(test_data))
+from chainer import iterators
 
 # class <クラス名> (<継承するクラス名>):
 class MLP (chainer.Chain):
@@ -45,8 +37,24 @@ class MLP (chainer.Chain):
 
 model = MLP()
 # 入力層のバイアスパラメータの配列の形
-print(model.layer1.b.shape)
+# print(model.layer1.b.shape)
 # 1000ユニット(パーセプトロン)
-print(model.layer1.b.data)
+# print(model.layer1.b.data)
 # 学習前の重み配列(None)
-print(model.layer1.W.array)
+# print(model.layer1.W.array)
+
+# MNIST(Mixed National Institute of Standards and Technology database)
+# 手書き数字のデータセットが70,000個ある
+# 28 x 28 = 784次元のデータ
+# scikit-learnの手書き数字は8 x 8のため, こちらの方がより特徴量が多くなっている
+train_data, test_data = chainer.datasets.get_mnist(withlabel = True, ndim = 1)
+# print(train_data)
+# print(test_data)
+# print(len(train_data))
+# print(len(test_data))
+
+# 学習用データと検証用データの反復子をデータセットを元に作成
+# SerialIteratorはChainerで最もシンプルなイテレータ
+BATCH_SIZE = 100
+train_iterator = iterators.SerialIterator(train_data, BATCH_SIZE)
+test_iterator = iterators.SerialIterator(test_data, BATCH_SIZE, repeat = False, shuffle = False)
