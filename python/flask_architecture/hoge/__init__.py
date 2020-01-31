5e8e701b222150e1b0dc7a9642fd31775e95c25c
Kazuki Tobita <kazukiti201@gmail.com>
2020-01-31 20:14:39 +0900

    Add SQLAlchemy

--- python/flask_architecture/hoge/__init__.py
+++ python/flask_architecture/hoge/__init__.py
@@ -1,8 +1,11 @@
 # アプリケーション本体
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('hoge.config')
+
+db = SQLAlchemy(app)
 
 import hoge.views
