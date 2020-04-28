8053ec338494b8d1bafeca0a8d780f0ba3d1d0c7
Kazuki Tobita <kazukiti201@gmail.com>
2020-04-28 20:31:43 +0900

    Modify comparison_operators

--- go/comparison_operators/main.go
+++ go/comparison_operators/main.go
@@ -9,4 +9,8 @@ func main()  {
 	// == 等しい, != 等しくない, < より小さい(未満), > より大きい(超過), <= 以下, >= 以上
 	var minor = age < 18	// false
 	fmt.Println(minor)
+
+	// 文字列の比較
+	// 辞書順で比較
+	fmt.Println("apple" < "banana")	// true
 }
